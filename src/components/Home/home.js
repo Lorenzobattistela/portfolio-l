375d55907c57587a695999edf6221185cef85020
Lorenzobattistela <lorenzowottrich@gmail.com>
2022-07-15 09:47:00 -0300

    typing carousel with skills

--- src/components/Home/home.js
+++ src/components/Home/home.js
@@ -1,8 +1,44 @@
+import "./home.scss";
+import React, { useState, useEffect } from "react"
+
 
 const Home = () => {
+    const [text, setText] = useState("")
+    const texts = ["JavaScript Developer.", "Open Source Contributor.", "Python Developer.", "Blockchain."]
+    const [fullText, setFullText] = useState(texts[0])
+    const [index, setIndex] = useState(0)
+    const [textIndex, setTextIndex] = useState(1)
+    
+    useEffect(() => {
+        if (index < fullText.length) {
+          setTimeout(() => {
+            setText(text + fullText[index])
+            setIndex(index + 1)
+          }, 100)
+        }
+        if(index == fullText.length){
+            setTimeout(()=> {
+                setIndex(0);
+                setText("")
+                setFullText(texts[textIndex]);
+                
+                if(textIndex < texts.length - 1){
+                    setTextIndex(textIndex + 1)
+                }
+                else {
+                    setTextIndex(0);
+                }
+            }, 1000)
+        }
+      }, [index])
+
+    
+    
     return(
-        <div>
-            <h1>ADSDASSDSDDD</h1>
+        <div className="home_container">
+            <h1>Hi there! </h1>
+            <h1>I am <span>Lorenzo Battistela</span></h1>
+            <h2>{text}</h2>
         </div>
     )
 }
