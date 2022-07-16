5094bc5da4972e5f0cdd23a0881574e221d20a80
Lorenzobattistela <lorenzowottrich@gmail.com>
2022-07-16 10:25:53 -0300

    creating footer

---
+++ src/components/Footer/footer.js
@@ -0,0 +1,13 @@
+import "./footer.scss";
+
+const Footer = () => {
+    return(
+        <div className="footer_container">
+            <h3>Design inspired on <a href="https://github.com/bobangajicsm/react-portfolio-website">this portfolio</a> and Developed by Lorenzo Battistela</h3>
+            <h3>Copyright © 2022 SB</h3>
+            <h3><a href="https://www.vecteezy.com/free-vector/developer">Developer Vectors by Vecteezy</a></h3>
+        </div>
+    )
+}
+
+export default Footer;
