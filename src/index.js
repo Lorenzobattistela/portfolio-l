c96e58041355322d4081fdaccf32ec4284a3ad4e
Lorenzobattistela <lorenzowottrich@gmail.com>
2022-07-19 13:27:16 -0300

    starting to develop projects route

--- src/index.js
+++ src/index.js
@@ -6,6 +6,7 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Layout from './components/Layout/layout';
 import Home from './components/Home/home';
 import Resume from "./components/Resume/resume";
+import Projects from './components/Projects/projects';
 
 export default function App() {
   return(
@@ -14,6 +15,7 @@ export default function App() {
         <Route path='/' element={<Layout/>}>
           <Route index element={<Home/>}/>
           <Route path="/resume" element={<Resume/>}/>
+          <Route path="/projects" element={<Projects/>}/>
         </Route>
       </Routes>
     </BrowserRouter>
