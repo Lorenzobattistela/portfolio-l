e6c3927d38f9013d139a32448124cbb4038be8b0
Lorenzobattistela <lorenzowottrich@gmail.com>
2022-07-21 17:27:16 -0300

    creating about component not finished

---
+++ src/components/About/about.js
@@ -0,0 +1,12 @@
+import './about.scss';
+
+const About = () => {
+    return(
+        <div className='about_container'>
+            <h1>Know who <span>I am</span></h1>
+            
+        </div>
+    )
+}
+
+export default About;
